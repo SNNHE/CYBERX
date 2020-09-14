837b6aa118f9f5c129ceccc706e1eac40033a789
Buu <songnana@cyberx.com>
2020-09-14 16:54:51 +0800

    cyberx - workers

--- dist/ServiceWorker/sw.js
+++ dist/ServiceWorker/sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'my-sw-v1'
 const urlsCache = [
-  './css/swiper-3.4.2.min.css',
-  './js/jquery.min.js',
-  './js/TweenMax.min.js',
-  './js/swiper-3.4.2.jquery.min.js',
-  './js/three.min.js',
-  './js/i18next.min.1.11.2.js',
+  '/css/swiper-3.4.2.min.css',
+  '/js/jquery.min.js',
+  '/js/TweenMax.min.js',
+  '/js/swiper-3.4.2.jquery.min.js',
+  '/js/three.min.js',
+  '/js/i18next.min.1.11.2.js',
 ]
 self.addEventListener('install', function(event) {
   event.waitUntil(
